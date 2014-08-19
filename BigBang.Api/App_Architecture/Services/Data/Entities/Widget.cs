7d4ab83e2edea923eed4122e14a41537a69bafa8
Matt Sell <matt.sell@improvingenterprises.com>
2014-08-19 18:43:48 -0500

    add sample entity and mapping

---
+++ BigBang.Api/App_Architecture/Services/Data/Entities/Widget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Highway.Data;
+
+namespace BigBang.Api.App_Architecture.Services.Data.Entities
+{
+    public class Widget : IIdentifiable<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BigBang.Api/App_Architecture/Services/Data/Mappings/WidgetConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using BigBang.Api.App_Architecture.Services.Data.Entities;
+
+namespace BigBang.Api.App_Architecture.Services.Data.Mappings
+{
+    public class WidgetConfiguration : EntityTypeConfiguration<Widget>
+    {
+        public WidgetConfiguration()
+        {
+            ToTable("Widgets");
+            HasKey(x => x.Id);
+            Property(x => x.Id).IsRequired();
+            Property(x => x.Name).IsRequired().HasMaxLength(150);
+        }
+    }
+}
