e9da4f65689cfd02181c17920f6dba3023210b81
Matt Sell <matt.sell@improvingenterprises.com>
2014-08-20 21:12:27 -0500

    add memebership reboot login controller

---
+++ BigBang.Api/App_Architecture/Installers/MembershipRebootInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Ef;
+using BrockAllen.MembershipReboot.WebHost;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace BigBang.Api.App_Architecture.Installers
+{
+    public class MembershipRebootInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                    Component.For<UserAccountService>(),
+                    Component.For<AuthenticationService>().ImplementedBy<SamAuthenticationService>(),
+                    Component.For<IUserAccountQuery>().ImplementedBy<DefaultUserAccountRepository>(),
+                    Component.For<IUserAccountRepository>()
+                    .ImplementedBy<DefaultUserAccountRepository>()
+                    .Named("BrockAllenUserAccountRepository")
+                    
+                );
+        }
+    }
+}
---
+++ BigBang.Api/Controllers/UserAccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BigBang.Api.Models;
+using BrockAllen.MembershipReboot;
+
+namespace BigBang.Api.Controllers
+{
+    public class UserAccountController : ApiController
+    {
+        UserAccountService _userAccountService;
+        AuthenticationService _authService;
+
+        public UserAccountController(AuthenticationService authService)
+        {
+            _userAccountService = authService.UserAccountService;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Login([FromBody] LoginInputModel model)
+        {
+            UserAccount account;
+            if (_userAccountService.AuthenticateWithUsernameOrEmail(model.Username, model.Password, out account))
+            {
+                _authService.SignIn(account, model.RememberMe);                
+            }
+            else
+            {
+                return Unauthorized();
+            }
+
+            return Ok();
+        }
+    }
+}
